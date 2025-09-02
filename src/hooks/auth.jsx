0b6e335f790368088ef2a1e23f3f4df0581565bc
Franco <francojesusgrieco@gmail.com>
2025-09-02 18:43:20 -0300

    Se agrego un formulario login

---
+++ src/hooks/auth.jsx
@@ -0,0 +1,92 @@
+import axios from "axios"
+import { createContext, useState } from "react"
+
+export const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [loggedIn, setLoggedIn] = useState(null)
+  const [loginForm, setLoginForm] = useState({
+    user: '',
+    password: ''
+  })
+
+  const [createForm, setCreateForm] = useState({
+    user: '',
+    password: ''
+  })
+
+  const updateCreateFormField = async (e) => {
+    const { value, name } = e.target
+    setCreateForm((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }))
+  }
+
+  const updateLoginFormField = (e) => {
+    const { name, value } = e.target
+    setLoginForm((prevData) => ({
+      ...prevData,
+      [name]: value.trim()
+    }))
+  }
+
+  const signup = async () => {
+    try {
+      await axios.post(import.meta.env.VITE_API_URL + 'signup', createForm, { withCredentials: true })
+      setCreateForm({ user: '', password: '' })
+    } catch (err) {
+      console.log('(signup):', err)
+      throw new Error('Error al crear el usuario', err)
+    }
+  }
+
+  const login = async () => {
+    try {
+      await axios.post(import.meta.env.VITE_API_URL + 'login', loginForm, {
+        withCredentials: true
+      })
+      console.log('hola')
+      setLoggedIn(true)
+      setLoginForm({ user: '', password: '' })
+    } catch (err) {
+      console.log('(login):', err)
+      throw new Error('Usuario o contraseña incorrectos')
+    }
+  }
+
+  const logout = async () => {
+    await axios.post(import.meta.env.VITE_API_URL + 'logout', {}, { withCredentials: true })
+    setLoggedIn(false)
+    setLoginForm({
+      user: '',
+      password: ''
+    })
+  }
+
+  const checkAuth = async () => {
+    try {
+      await axios.get(import.meta.env.VITE_API_URL + 'check-auth', { withCredentials: true })
+      setLoggedIn(true)
+    } catch (err) {
+      setLoggedIn(false)
+      console.log('(checkAuth)Se ha producido un error:', err)
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{
+      loginForm,
+      createForm,
+      loggedIn,
+      signup,
+      login,
+      logout,
+      updateLoginFormField,
+      updateCreateFormField,
+      checkAuth
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,54 @@
-import { Button, TextField } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Button, Container, TextField, Typography } from "@mui/material";
+import { useContext } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../hooks/auth";
 
-export default function Login(){
-  return(
-    <>
-      <TextField id='username' label='Usuario'></TextField>
-      <TextField id='password' label='Contraseña'></TextField>
-      <Button component={Link} to={'/'}>Ingresar</Button>
-    </>
+export default function Login() {
+
+  const navigate = useNavigate()
+  const {loginForm, updateLoginFormField, login} = useContext(AuthContext)
+  const [error, setError] = useState('')
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    try {
+      await login()
+      setError(null)
+      navigate('/')
+    } catch (err) {
+      setError(err.message || 'Error al iniciar sesión')
+    }
+  }
+  if (!loginForm) return <></>
+  return (
+    <Container
+      component='form'
+      onSubmit={handleLogin}
+      maxWidth='sm'
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '10px'
+      }}>
+      <Typography variant='h4'>Login</Typography>
+
+      <TextField
+        name='user'
+        label='Usuario'
+        type='text'
+        value={loginForm.user || ''}
+        onChange={updateLoginFormField}
+      />
+      <TextField
+        name='password'
+        label='Contraseña'
+        type='password'
+        value={loginForm.password || ''}
+        onChange={updateLoginFormField}
+      />
+      {error && <Typography color="error">{error}</Typography>}
+      <Button type='submit'>Login</Button>
+    </Container>
   )
 }

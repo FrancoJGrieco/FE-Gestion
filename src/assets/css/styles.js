5fa33e8b0a5447a1b5bda77120cf12ffb60d4ca1
Franco <francojesusgrieco@gmail.com>
2025-10-01 16:14:49 -0300

    se agrego un archivo de estilos

---
+++ src/assets/css/styles.js
@@ -0,0 +1,31 @@
+export const styles = {
+  // Estilos de ProductoCard
+  bxCard: {
+    backgroundColor: "darkgray",
+    borderRadius: 0.5,
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 1
+  },
+  bxCardElement: {
+    color: 'black',
+    textDecoration: 'none',
+    backgroundColor: 'gray',
+    borderTopLeftRadius: 2,
+    borderTopRightRadius: 2,
+    height: '2rem',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  bxCardDes: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 1,
+    marginBottom: 1
+  },
+  bxCardInfo: {
+    marginLeft: 1
+  }
+}
--- src/components/ProductoCard.jsx
+++ src/components/ProductoCard.jsx
@@ -2,49 +2,27 @@ import { Box, IconButton, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import removeDatum from "../services/removeDatum";
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import { styles } from '../assets/css/styles'
 
 export default function ProductoCard({ producto }) {
 	const type = 'productos'
 	return (
 		<Box
-			sx={{
-				backgroundColor: "darkgray",
-				borderRadius: 0.5,
-				display: 'flex',
-				flexDirection: 'column',
-				gap: 1
-			}}
+			sx={styles.bxCard}
 		>
 			<Box
 				component={Link}
 				to={`/productos/update/${producto.id}`}
 				state={{ id: producto.id }}
-				sx={{
-					textDecoration: 'none',
-					backgroundColor: 'gray',
-					borderTopLeftRadius: 2,
-					borderTopRightRadius: 2,
-					height: '2rem',
-					display: 'flex',
-					flexDirection: 'row',
-					justifyContent: 'center',
-					alignItems: 'center'
-				}}
+				sx={styles.bxCardElement}
 			>
 				<Typography>{producto.name}</Typography>
 			</Box>
 			<Box
-				sx={{
-					display: 'flex',
-					flexDirection: 'row',
-					gap: 1,
-					marginBottom: 1
-				}}
+				sx={styles.bxCardDes}
 			>
 				<Box
-					sx={{
-						marginLeft: 1
-					}}
+					sx={styles.bxCardInfo}
 				>
 					<Typography>
 						Codigo: {producto.codigo}
